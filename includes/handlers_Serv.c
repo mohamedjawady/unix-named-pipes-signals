2ad5ffdc288464bd4a7f2fd8c563af6180c10104
Mohamed Habib Jaouadi <mohamedhabib.jaouadi@etudiant-isi.utm.tn>
2023-10-27 18:44:56 +0100

    added server handlers implementations

---
+++ includes/handlers_Serv.c
@@ -0,0 +1,20 @@
+#include "Handlers_Serv.h"
+#include "utils.h"
+
+void fin_conversation(int sig)
+{
+    debugMessage("Received signal from client, waking up...");
+    printf("----------------------------------------------------------\n");
+    fflush(stdout);
+}
+
+void fin_serveur(int sig)
+{
+    char msg[50];
+    sprintf(msg, "Received signal %d from client", sig);
+    debugMessage(msg);
+    printf("Shutting down...\n");
+    printf("----------------------------------------------------------\n");
+    fflush(stdout);
+    exit(sig);
+}
